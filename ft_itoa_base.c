2adb757f71c95660d08aa1830ad9802ca74bec6b
cphillip <cphillip@student.hive.fi>
2020-06-15 14:28:33 +0300

    norm changes

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csphilli <csphilli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 11:40:26 by cphillip          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2020/06/15 13:17:46 by cphillip         ###   ########.fr       */
-=======
-/*   Updated: 2020/03/07 09:36:12 by csphilli         ###   ########.fr       */
->>>>>>> 006a08b2f03ffdd40a43c186d0abfc9cdd07d260
+/*   Updated: 2020/06/15 14:28:07 by cphillip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +16,7 @@ char	*ft_itoa_base(uintmax_t nbr, int base)
 {
 	char	*new;
 	int		j;
-<<<<<<< HEAD
-
-=======
->>>>>>> 006a08b2f03ffdd40a43c186d0abfc9cdd07d260
+	
 	j = ft_nbr_size_base(nbr, base);
 	if (!(new = (char*)malloc(sizeof(char) * j + 1)))
 		return (NULL);
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csphilli <csphilli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 12:25:51 by cphillip          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2020/06/15 13:18:48 by cphillip         ###   ########.fr       */
-=======
-/*   Updated: 2020/03/06 23:55:30 by csphilli         ###   ########.fr       */
->>>>>>> 006a08b2f03ffdd40a43c186d0abfc9cdd07d260
+/*   Updated: 2020/06/15 14:28:08 by cphillip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +22,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
-<<<<<<< HEAD
 	if (!(new = (char*)malloc(sizeof(char)\
 		* (ft_strlen(s1) + ft_strlen(s2) + 1))))
-=======
-	if (!(new = (char*)malloc(sizeof(char) *(ft_strlen(s1) + ft_strlen(s2) + 1))))
->>>>>>> 006a08b2f03ffdd40a43c186d0abfc9cdd07d260
 		return (NULL);
 	while (s1[i])
 		new[j++] = s1[i++];
