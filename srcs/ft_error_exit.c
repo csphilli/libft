183bea058cb503acf14e1ca058d48f32347d4bee
Christopher <csphilli@gmail.com>
2021-01-15 19:56:35 +0200

    fixed typo

--- srcs/ft_error_exit.c
+++ srcs/ft_error_exit.c
@@ -6,13 +6,13 @@
 /*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 15:17:11 by cphillip          #+#    #+#             */
-/*   Updated: 2021/01/15 19:51:19 by cphillip         ###   ########.fr       */
+/*   Updated: 2021/01/15 19:56:20 by cphillip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_handle_error(char *error_msg)
+void	ft_errorexit(char *error_msg)
 {
 	write(2, error_msg, ft_strlen(error_msg));
 	exit(-1);
