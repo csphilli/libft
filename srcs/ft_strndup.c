b0f6afb4ed2b57377ba7db720603e985942b38ca
Christopher Phillips <csphilli@gmail.com>
2021-03-11 16:41:37 +0200

    updated ft_strndup. removed uneccessary line

--- srcs/ft_strndup.c
+++ srcs/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: csphilli <csphilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 13:17:02 by cphillip          #+#    #+#             */
-/*   Updated: 2021/01/29 18:14:02 by csphilli         ###   ########.fr       */
+/*   Updated: 2021/03/11 16:41:13 by csphilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@ char	*ft_strndup(char *str, int size)
 	char	*new;
 
 	new = ft_strnew(sizeof(char*) * size);
-	new[size + 1] = '\0';
 	return ((char*)ft_memcpy(new, str, size));
 }
