bfe6429c665c532514f89d09327b8d56e354a71f
Christopher Phillips <csphilli@gmail.com>
2021-04-24 22:39:13 +0300

    new normed

--- srcs/ft_free_strsplit.c
+++ srcs/ft_free_strsplit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_free_strsplit.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: csphilli <csphilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/23 11:55:45 by cphillip          #+#    #+#             */
-/*   Updated: 2020/12/16 11:43:04 by cphillip         ###   ########.fr       */
+/*   Updated: 2021/04/24 22:38:59 by csphilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	free_strsplit(char ***array)
 {
-	char **current;
+	char	**current;
 
 	if (array && *array)
 	{
